1709678e5e4ab1c30e181b780641993b232d8b33
James Traver <jtraverjr@gmail.com>
2015-08-19 14:05:33 -0500

    yay

--- app.rb
+++ app.rb
@@ -2,25 +2,21 @@ require 'bundler'
 Bundler.require
 
 get '/' do
-
   erb :index
-
 end
 
 get '/1' do
-
   erb :one
-
 end
 
 get '/2' do
-
   erb :two
-
 end
 
 get '/3' do
-
   erb :three
+end
 
+get '/4' do
+  erb :four
 end
